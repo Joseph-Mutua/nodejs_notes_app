4c8a229fc503b95f2e2fa7c483e3b2a3a70dac0d
Joseph Mutua <mutuaj793@gmail.com>
2020-07-14 13:32:03 +0000

    Add ots.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const add = require('./utils.js');
+const getNotes = require('./notes.js');
+
+const sum = add(4, -3);
+
+console.log(sum);
+console.log(getNotes());
---
+++ utils.js
@@ -0,0 +1,9 @@
+console.log("Utils");
+
+const fname = "Mike";
+
+const add = function (a, b) {
+    return a + b;
+}
+
+module.exports = add;
