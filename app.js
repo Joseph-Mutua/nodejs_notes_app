e4f25fc7cfd1beebf43e17ba363b0eed4878f831
Joseph Mutua <mutuaj793@gmail.com>
2020-07-14 14:40:05 +0000

    Add odemon functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const getNotes = require('./notes.js');
 
 const log = console.log;
 
-log(chalk.blue("Success!", "Wwwwwooow", chalk.underline.bgRed("Mutua!!")));
+log(chalk.blue("Success!", "Wwwwwooow", chalk.underline("Mutua!!")));
+log(chalk.red.bold("Error"))
+
 log(getNotes());
 log(validator.isURL('fuktrrrumpandcruz.io'));
