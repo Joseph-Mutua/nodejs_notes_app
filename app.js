ceb32cfb2ec18f8ffc3fbd8472908bdd1164c4eb
Joseph Mutua <mutuaj793@gmail.com>
2020-07-14 14:22:48 +0000

    Add chalk library

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
+const log = console.log;
 
-console.log(getNotes());
-console.log(validator.isURL('fuktrrrumpandcruz.io'));
+log(chalk.blue("Success!", "Wwwwwooow", chalk.underline.bgRed("Mutua!!")));
+log(getNotes());
+log(validator.isURL('fuktrrrumpandcruz.io'));
